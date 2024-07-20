254198d4bea5388f17f71e8f97ecf916d6cc73f4
Abhinav Nayak <46524231+abhinavnayak21@users.noreply.github.com>
2024-07-20 19:12:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const userRoutes = require("./routes/userRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 require("dotenv").config();
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
 
 // parse application/json
 app.use(bodyParser.json());
 
-// app.use(cors());
-// // Enable All CORS Requests (for demonstration purposes)
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specified methods
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
-//   next();
-// });
+app.use(cors());
+// Enable All CORS Requests (for demonstration purposes)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specified methods
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+  next();
+});
+
 
 app.use("/comment", commentRoutes);
 app.use("/blog", blogRoutes);
